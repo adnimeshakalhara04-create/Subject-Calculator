be765d405140e1060a9936138175596a618fae37
A.D Nimesha Kalhara <a.dnimeshakalhara04@gmail.com>
2026-09-20 13:22:00 +0530

    Add files via upload

---
+++ merge.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Merging parts into Subject Calculator.zip ...
+copy /b "Subject Calculator.zip.part001"+"Subject Calculator.zip.part002"+"Subject Calculator.zip.part003"+"Subject Calculator.zip.part004"+"Subject Calculator.zip.part005"+"Subject Calculator.zip.part006"+"Subject Calculator.zip.part007" "Subject Calculator.zip"
+echo Done! File merged as: Subject Calculator.zip
+pause
